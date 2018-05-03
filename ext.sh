89681249d0a09ed5cd6a8003470ac25ecbdba130
fang.qi <tomatos>
2018-05-03 17:39:20 +0800

    init

---
+++ ext.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# only works in ENTRYPOINT
+# if [ ! -z "$MYSQL_EXT_LOWER_CASE_TABLE_NAMES" ] && [ -f /etc/mysql/conf.d/mysql.cnf ]; then
+#     echo "lower_case_table_names = $MYSQL_EXT_LOWER_CASE_TABLE_NAMES" >> /etc/mysql/conf.d/mysql.cnf
+# fi
